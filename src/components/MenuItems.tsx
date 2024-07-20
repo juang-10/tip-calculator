3b28d983ef66aaaac7807226a3b9fb57f4b65816
david <judagosa8520@gmail.com>
2024-07-20 13:38:16 -0500

    feat: Add addItem function to MenuItems component

--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -1,13 +1,15 @@
 import { MenuItem } from "../types"
 
 type MenuItemProps = {
-  item: MenuItem
+  item: MenuItem,
+  addItem: () => void,
 }
 
-export const MenuItems = ({ item } : MenuItemProps) => {
+export const MenuItems = ({ item, addItem } : MenuItemProps) => {
   return (
     <button
       className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+      onClick={() => addItem()}
     >
       <p>{ item.name }</p>
       <p className="font-black">${ item.price }</p>
