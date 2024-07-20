bf776a3b10622f30629d3492afcaacddae561914
david <judagosa8520@gmail.com>
2024-07-20 12:26:04 -0500

    chore: Add menu layout structure to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,16 @@ function App() {
       <header className=" bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Tip calculator and inquiries</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+        </div>
+
+        <div>
+          <h2>Consumption</h2>
+        </div>
+      </main>
     </>
   )
 }
