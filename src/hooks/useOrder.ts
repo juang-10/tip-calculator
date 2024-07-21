ee3025ccca5eee3662b5de2ce392f280c062b00b
david <judagosa8520@gmail.com>
2024-07-21 16:22:28 -0500

    feat: Update useOrder hook to handle adding items to the order

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,15 +1,27 @@
 import { useState } from "react"
-import { OrderItem } from "../types";
+import { MenuItem, OrderItem } from "../types";
 
 export const useOrder = () => {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
-  const addItem = () => {
-    console.log('Agregando...')
+  const addItem = (item: MenuItem) => {
+    const itemeExists = order.find(orderItem => orderItem.id === item.id)
+    if (itemeExists) {
+      const updateOrder = order.map(
+        orderItem => orderItem.id === item.id 
+          ? {...item, quantity: orderItem.quantity + 1} 
+          : orderItem
+      )
+      setOrder(updateOrder);
+    } else {
+      const newItem = {...item, quantity: 1}
+      setOrder([...order, newItem])
+    }
   }
+
+  console.log({order})
   
   return {
     addItem,
-    
   }
 }
