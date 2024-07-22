6f31c4da210d1b37f73411dc20fde597d7253130
david <judagosa8520@gmail.com>
2024-07-21 19:38:51 -0500

    feat: add functionality to delete an order item

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,8 +5,8 @@ export const useOrder = () => {
   const [order, setOrder] = useState<OrderItem[]>([]);
 
   const addItem = (item: MenuItem) => {
-    const itemeExists = order.find(orderItem => orderItem.id === item.id)
-    if (itemeExists) {
+    const itemExists = order.find(orderItem => orderItem.id === item.id)
+    if (itemExists) {
       const updateOrder = order.map(
         orderItem => orderItem.id === item.id 
           ? {...item, quantity: orderItem.quantity + 1} 
@@ -19,8 +19,27 @@ export const useOrder = () => {
     }
   }
 
+  const removeItem = (id: MenuItem['id']) => {
+    const idExists = order.find(item => item.id === id);
+    
+    if(idExists && idExists?.quantity > 1 ){
+      const updateOrder = order.map(
+        orderItem => orderItem.id === idExists.id 
+          ? {...idExists, quantity: orderItem.quantity - 1} 
+          : orderItem
+      )
+      console.log({updateOrder})
+      setOrder(updateOrder);
+    } else {
+      const newOrder = order.filter((item) => item.id !== id);
+      console.log("🚀 ~ removeItem ~ newOrder:", newOrder)
+      setOrder( newOrder );
+    }
+  }
+
   return {
     order,
     addItem,
+    removeItem
   }
 }
