6b8ad486b1144a181034f8ce95403cfa9973cca1
david <judagosa8520@gmail.com>
2024-07-23 22:27:00 -0500

    refactor: Simplify rendering logic for OrderContent component

--- src/components/OrderContent.tsx
+++ src/components/OrderContent.tsx
@@ -12,33 +12,27 @@ export const OrderContent = ({ order, removeItem }: OrderContentProps) => {
       <h2 className="text-4xl font-black">Consumption</h2>
 
       <div className="space-y-3 mt-10">
-        { order.length === 0 
-          ? <p className="text-center">The order is empty</p>
-            
-          : (
-            order.map(item => (
-              <div 
-                key={item.id}
-                className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
-              >
-                <div>
-                  <p className="text-lg">
-                    {item.name} - {formatCurrency(item.price)}
-                  </p>
-                  <p className="font-black">
-                    Quantity: {item.quantity} - {formatCurrency(item.price * item.quantity)}
-                  </p>
-                </div>
-                <button 
-                  className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
-                  onClick={() => removeItem(item.id)}
-                >
-                  X
-                </button>
-              </div>
-            ))
-          )
-        }
+        { order.map(item => (
+          <div 
+            key={item.id}
+            className="flex justify-between items-center border-t border-gray-200 py-5 last-of-type:border-b"
+          >
+            <div>
+              <p className="text-lg">
+                {item.name} - {formatCurrency(item.price)}
+              </p>
+              <p className="font-black">
+                Quantity: {item.quantity} - {formatCurrency(item.price * item.quantity)}
+              </p>
+            </div>
+            <button 
+              className="bg-red-600 h-8 w-8 rounded-full text-white font-black"
+              onClick={() => removeItem(item.id)}
+            >
+              X
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   )
